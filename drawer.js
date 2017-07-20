5c3c3046749f97bc14214bd71a993ffc8b928cf1
Eliseo Geraldo <e10169610@gmail.com>
2017-07-20 00:14:36 -0500

    Trigger bindeable
    Now yout can bind an DOM's element for use as a trigger button

--- drawer.js
+++ drawer.js
@@ -55,14 +55,43 @@ document.addEventListener('DOMContentLoaded', function() {
 				Drawer.$modal.style.transition = 'none';
 			}, 200);
 		},
+		controlDrawer: function() {
+			this.$drawer.style.transition = '0.2s ease';
+			this.$modal.style.transition = '0.2s ease';
+			if ( this.$drawer.classList.contains('actived')) {
+				this.$drawer.classList.remove('actived')
+				this.$drawer.style.left = '-300px';
+				this.$modal.style.opacity = '0';
+				setTimeout(function() {
+					Drawer.$modal.style.display = 'none';
+					Drawer.$modal.style.transition = 'none';
+				}, 200);
+			} else {
+				this.$drawer.classList.add('actived')
+				this.$drawer.style.left = '0px';
+				this.$modal.style.display = 'block';
+				this.$modal.style.opacity = '1';
+			}
+
+			setTimeout(function() {
+				Drawer.$drawer.style.transition = 'none';
+				Drawer.$modal.style.transition = 'none';
+			}, 200);
+		},
 		bindListener: function() {
 			window.addEventListener('touchstart', this.startDraggingMenu.bind(this));
 			window.addEventListener('touchend', this.disableDraggingMenu.bind(this));
 			window.addEventListener('touchmove', this.draggingMenu.bind(this));
+			try {
+				this.$trigger.addEventListener('click', this.controlDrawer.bind(this));
+			} catch (err) { console.log(err) }
 		},
 		cacheDOM: function() {
 			this.$drawer = document.getElementById('drawer_js');
 			this.$modal = document.querySelector('.__modal_drawer');
+			try {
+				this.$trigger = document.getElementById('drawer_trgger');
+			} catch (err) { console.log(err) }
 		}
 	};
 
